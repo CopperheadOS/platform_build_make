6fef7134938b784357a842b58870029f1bc81a24
David Klein <david.klein@copperhead.co>
2019-11-24 23:46:08 -0500

    Remove sepolicy common/ directory
    Change-Id: I5960c4c01a840542803524a4320413b677c61023

--- core/config.mk
+++ core/config.mk
@@ -1183,10 +1183,10 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
-ifneq ($(wildcard device/copperhead/sepolicy/common/sepolicy.mk),)
+ifneq ($(wildcard device/copperhead/sepolicy/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/copperhead/sepolicy/common/sepolicy.mk)
+$(eval include device/copperhead/sepolicy/sepolicy.mk)
 endif
 
 # Include any vendor specific config.mk file
